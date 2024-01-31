5b8bdbabecd70aef12af6308865089b1d40cd1f3
unknown <jan.tomcza@zsegw.pl>
2024-01-31 14:54:24 +0100

    articles

--- Kalkulator/obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
+++ Kalkulator/obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1eb8823a5003f210d0e92c2ad68dbe75f00d07b")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
